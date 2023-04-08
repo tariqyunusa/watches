03834c06e51ff28ef151d98ac41fc75d1d6d7ab1
tariq yunusa <tariikhyunusa@gmail.com>
2023-04-08 16:32:42 +0100

    added the title func,but no loop

--- images/script.js
+++ images/script.js
@@ -4,10 +4,11 @@ show_image(index);
 
 function show_image(i){
     index += 1;
-
+    var year = document.getElementById("year");
     var images = document.getElementsByClassName("image");
-    
-
+    var title = document.getElementById('title');
+    const watchName = ["RAZANETTI", "SANIEGE"];
+    const years = ["1971", "1984"];
 
     for(i = 0; i < images.length; i++)
         images[i].style.display = "none"
@@ -19,4 +20,16 @@ function show_image(i){
     index = images.length - 1;
 
     images[index].style.display = "block";
+    title.innerHTML = watchName[index++];
+    year.textContent = years[index++];
+
+    for(j = 0; j <title.length; j++)
+        
+
+    if(index > title.length -1)
+        index = 0;
+
+    if(index < 0)
+        index = title.length -1;
+
 }
